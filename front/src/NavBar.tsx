1930dc7fe1b82b1e3745cac2e09c3cd21d9cc5e3
L4l1nd3 <alvaro.lalinde@live.u-tad.com>
2020-04-26 19:13:58 +0200

    Added split button for profile options

--- front/src/NavBar.tsx
+++ front/src/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
 
-import {Navbar,NavDropdown, Form, FormControl} from 'react-bootstrap'
+import {Navbar,NavDropdown, Form, FormControl, Dropdown} from 'react-bootstrap'
 
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +12,10 @@ import './NavBar_styles.css';
 
 import axios from "axios";
 import history from './history';
+
+import DropdownButton from 'react-bootstrap/DropdownButton'
+
+import SplitButton from 'react-bootstrap/SplitButton'
 
 export class NavBar extends Component {
 
@@ -58,31 +62,30 @@ export class NavBar extends Component {
                                     <Nav.Link href="/Chat/">Chat</Nav.Link>
                                 </Nav>
                                 <Nav className=" navbar-box-2">
-                                    <NavDropdown title={<i className="far fa-user fa-sg icon-profile profile-dropdown"></i>} id="basic-nav-dropdown">
+                                    <SplitButton href="/" variant="secondary" alignRight title={<i className="far fa-user fa-sg icon-profile profile-dropdown"></i>} id="dropdown-menu-align-right">
                                         <NavDropdown.Item href="/">Settings</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                        <NavDropdown.Item href="/">View profile</NavDropdown.Item>
-                                    <NavDropdown.Divider />
                                         <NavDropdown.Item  onClick={() => this.logout()}>Log out</NavDropdown.Item>
-                                    </NavDropdown>
+                                    </SplitButton>
                                     <Nav.Link className="logout-icon" onClick={() => this.logout()}> <i className="fas fa-running fa-lg "></i> </Nav.Link>
                             
                                 </Nav>
                             </Navbar.Collapse>
                             :
                             <Navbar.Collapse id="responsive-navbar-nav" > 
-                            <Nav className="navbar-box-1">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/">About us</Nav.Link>
-                            </Nav>
-                            <Nav className=" navbar-box-2">
-                                <NavDropdown title={<i className="far fa-user fa-sg icon-profile profile-dropdown"></i>} id="basic-nav-dropdown">
-                                
-                                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                                    <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        </Navbar.Collapse>
+                                <Nav className="navbar-box-1">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/">About us</Nav.Link>
+                                </Nav>
+                                <Nav className=" navbar-box-2">
+                                    <NavDropdown alignRight title={<i className="far fa-user fa-sg icon-profile-gray profile-dropdown"></i>} id="dropdown-menu-align-right">
+                                    
+                                        <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                                        <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+                                    </NavDropdown>
+
+                                </Nav>
+                            </Navbar.Collapse>
                         }
             </Navbar>
 
